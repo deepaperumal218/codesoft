01fc8075220679a168186808accd583c8deb2014
deepaperumal218 <164529096+deepaperumal218@users.noreply.github.com>
2025-02-04 19:32:37 +0530

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Grade {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the number of subjects
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        // Array to store marks
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+
+        // Input marks
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + (i + 1) + " (out of 100): ");
+            marks[i] = scanner.nextInt();
+            totalMarks += marks[i];
+        }
+
+        // Calculate average percentage
+        double averagePercentage = (double) totalMarks / numSubjects;
+
+        // Determine grade
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        // Display results
+        System.out.println("\n--- Results ---");
+        System.out.println("Total Marks: " + totalMarks + "/" + (numSubjects * 100));
+        System.out.println("Average Percentage: " + String.format("%.2f", averagePercentage) + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
